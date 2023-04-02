0128fe381795553a4371cb4503fefbf203984764
costillap <costillap@hotmail.com.ar>
2023-04-02 02:54:39 -0300

    validacion formulario

--- validacion.js
+++ validacion.js
@@ -1,1 +1,54 @@
 //Haz tú validación en javascript acá
+const inputs = document.querySelectorAll("input, textarea");
+
+inputs.forEach((input) =>{
+    input.addEventListener("blur", (input) =>{
+        valida(input.target);
+    });
+ });
+
+ function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+ };
+
+ const validadores = {
+    nombre: (input) => validarNonbre(input),
+    email: (input) => validarEmail(input),
+    asunto: (input) => validarAsunto(input),
+    textarea: (input) => validarTextArea(input)
+ }
+
+ function validarNonbre(input){
+    if (input.value.length < 5){
+        document.getElementById("span__Name").classList.remove("span--disable");
+        
+    }else{
+        document.getElementById("span__Name").classList.add("span--disable");
+    }
+}
+ function validarEmail(input){
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (!regexEmail.test(input.value)){
+        document.getElementById("span__Email").classList.remove("span--disable");
+    }else{
+        document.getElementById("span__Email").classList.add("span--disable");
+    }
+ }
+ function validarAsunto(input){
+    if (input.value.length < 5){
+        document.getElementById("span__Asunto").classList.remove("span--disable");
+        
+    }else{
+        document.getElementById("span__Asunto").classList.add("span--disable");
+    }
+ }
+ function validarTextArea(input){
+    if (input.value.length<25){
+        document.getElementById("span__TextArea").classList.remove("span--disable");
+    }else{
+        document.getElementById("span__TextArea").classList.add("span--disable");
+    }
+ }
